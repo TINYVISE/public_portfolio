8eacfd4a3f6eba2550eb8af6e56e9733668edaaf
Juan Gutierrez <gutierr3z@gmail.com>
2020-12-30 11:34:18 -0500

    added css and js folders and main files

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>t i n y v i s e</title>
+
+    <link href="css/main.css" rel="stylesheet">
 </head>
 <body>
     hello
+
+    <script src="js/main.js"></script>
 </body>
 </html>
