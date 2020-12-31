e6f04232b5a38687cc9a6a8b9c9a4bfdb9bcd5be
Juan Gutierrez <gutierr3z@gmail.com>
2020-12-31 12:13:10 -0500

    setting up project gallery setup in js, using dom scripting to generate elements

--- js/main.js
+++ js/main.js
@@ -11,17 +11,26 @@ burger.addEventListener( 'click', () => {
 // gallery home
 
 let homeTiles = document.querySelectorAll( '#gallery-home > div' );
+let dump = document.getElementById( 'dump' );
 
 homeTiles.forEach( function( item ) {
     item.addEventListener( 'click', ( e ) => {
         galleryProject.classList.add( 'open' );
+        dump.innerHTML = item.dataset.layout;
+        if( item.dataset.layout === 'gallery' ) {
+            galleryBuilder( 15 );
+        }
+        
     });
+
+    
 });
 
 // gallery project
 
 let galleryProject = document.getElementById( 'gallery-project' );
 let galleryProjectClose = document.getElementById( 'gallery-project-close' );
+
 
 galleryProjectClose.addEventListener( 'click', ( e ) => {
     galleryProject.classList.remove( 'open' );
@@ -44,3 +53,20 @@ function changeOnScroll( el, cssclass ) {
 }
 
 changeOnScroll( header, 'active' );
+
+// gallery builder
+
+// let dump = document.getElementById( 'dump' );
+
+function galleryBuilder( num_of_slides ) {
+    
+    let gridWrapper = document.createElement( 'div' );
+    gridWrapper.setAttribute( 'class', 'gallery-wrapper' );
+    for( let i = 0; i < num_of_slides; i++ ) {
+        let slideDiv = document.createElement( 'div' );
+        slideDiv.setAttribute( 'class', 'slide' );
+        gridWrapper.appendChild( slideDiv );
+    }
+
+    dump.appendChild( gridWrapper );
+}
